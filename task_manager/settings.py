7cdf2fae54761bee8db0e37626afb86ff85a2fbc
Sergey Kornienko <skornienko29@gmail.com>
2021-04-25 21:36:58 +0300

    step 3 auth test

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -25,7 +25,7 @@ if os.path.exists(dotenv_path):
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'oweugnweufgnw')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', True)
