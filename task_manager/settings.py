6afd8c73d463ab48edf1d8fbff9a599987da7adc
Sergey Kornienko <skornienko29@gmail.com>
2021-05-18 00:18:50 +0300

    step 5

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,7 +28,7 @@ if os.path.exists(dotenv_path):
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = [
     'localhost',
